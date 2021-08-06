9c830221cde0ed6472862fff26ab2aa829adefdf
gkwlal3 <84840627+gkwlal3@users.noreply.github.com>
2021-08-06 17:39:20 +0900

    Add files via upload

---
+++ ServletTest/src/main/java/co/micol/servletTest/First.java
@@ -0,0 +1,35 @@
+package co.micol.servletTest;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/First")
+public class First extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public First() {
+        super();
+        // TODO Auto-generated constructor stub 부모생성자로 부터
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append("나의 이름을 찍어보자");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
