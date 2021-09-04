a101ff7aed8a903d2ae231b850c0b84b1b80cc1f
hojun <84840627+hojunking@users.noreply.github.com>
2021-09-04 12:41:49 +0900

    Delete 20210726 directory
    무쓸모

--- 20210726/src/main/java/co/yedam/board/command/Command.java
+++
@@ -1,9 +0,0 @@
-package co.yedam.board.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Command {
-	public String execute(HttpServletRequest request ,HttpServletResponse response);
-
-}
